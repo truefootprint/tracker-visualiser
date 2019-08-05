0bef2246b62a7e4734fcd1fb84afba063a64bebc
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-05 11:56:14 +0100

    Add a Client for speaking to the backend

---
+++ helpers/client.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+class Client {
+  constructor(host) {
+    this.host = host;
+  }
+
+  companyRankings(sector, year, { type, id }) {
+    return this.get(`/company_rankings/${sector}-${year}-${type}-${id}`);
+  }
+
+  ancestry({ type, id }) {
+    return this.get(`/ancestry/${type}/${id}`);
+  }
+
+  get(path) {
+    return axios.get(`${this.host}${path}`);
+  }
+}
+
+export default Client;
