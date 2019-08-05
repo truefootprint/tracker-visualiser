117a1b316a99195deb46ac565e71f77ccd66586d
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-05 12:01:14 +0100

    Move the Graph HTML into the React component
    This still needs some tidying but it’s a step in
    the right direction.

--- components/graph/Graph.jsx
+++ components/graph/Graph.jsx
@@ -1,29 +1,118 @@
-import Head from "next/head";
-import DrillDown from "../drill_down";
-import Breadcrumbs from "../breadcrumbs";
+import * as d3 from "d3";
+import { useEffect } from "react";
 
-const Graph = () => {
-  if (typeof document !== "undefined") {
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "/static/graph.js";
-    document.body.appendChild(s);
-  }
+const Graph = ({ rankings }) => {
+  useEffect(() => {
+    d3.select('svg').html("");
 
-  return <>
-    <Head>
-      <link rel="stylesheet" type="text/css" href="/static/graph.css" />
-    </Head>
+    // Optional things we can set:
+    const optionalMin = null;
+    const optionalMax = null;
+    const axisLabel = rankings.name;
+    //
 
-    <Breadcrumbs />
-    <p>-----------</p>
-    <DrillDown />
+    const companies = rankings.ranked_companies.filter(c => c.value !== null);
+    const nulls = rankings.ranked_companies.filter(c => c.value === null);
 
-    <h1 className="title"></h1>
-    <svg width="1000" height="600" />
-    <p className="nulls"></p>
-    <script src="https://d3js.org/d3.v5.min.js"></script>
-  </>
+    d3.select('.nulls').text(nulls.map(n => n.name).join(", "));
+
+    const data = companies.map(d => ({
+      name: d.name,
+      value: d.value.toPrecision(3),
+      band: d.band,
+    }));
+
+    const bandColors = ["#9f9", "#fc9", "#f99"];
+    const highlights = ["#3f3", "#fc3", "#f33"];
+
+    const names = data.map(d => d.name);
+    const values = data.map(d => d.value);
+
+    const [_, max] = d3.extent(values);
+
+    const margin = 80;
+    const width = 1000 - 2 * margin;
+    const height = 600 - 2 * margin;
+
+    const svg = d3.select('svg');
+
+    const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+
+    const axisMin = optionalMin || 0;
+    const axisMax = optionalMax || max;
+
+    const xScale = d3.scaleLinear().range([0, width]).domain([axisMin, axisMax]);
+    const yScale = d3.scaleBand().range([0, height]).domain(names).padding(0.4);
+
+    const translateAxis = `translate(0, ${height})`;
+
+    const handleMouseOver = (d, index) => {
+      chart.selectAll(`*[data-index='${index}']`)
+        .attr('class', 'highlighted');
+    };
+
+    const handleMouseOut = (d, index) => {
+      chart.selectAll(`*[data-index='${index}']`)
+        .attr('class', '');
+    };
+
+    chart.append('g').call(d3.axisLeft(yScale));
+
+    chart.append('g')
+        .attr('transform', translateAxis)
+        .call(d3.axisBottom(xScale));
+
+    svg.append('text')
+          .attr('x', width / 2 + margin)
+          .attr('y', height + margin * 1.7)
+          .attr('text-anchor', 'middle')
+          .text(axisLabel)
+
+    chart.append('g')
+        .attr('transform', translateAxis)
+        .call(d3.axisBottom()
+            .scale(xScale)
+            .tickSize(-height, 0, 0)
+            .tickFormat(''))
+
+    chart.selectAll()
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('data-index', (_, i) => i)
+        .attr('data-band', d => d.band)
+        .attr('y', d => yScale(d.name))
+        .attr('height', yScale.bandwidth())
+        .on('mouseover', handleMouseOver)
+        .on('mouseout', handleMouseOut)
+        .transition()
+        .duration(500)
+        .delay((_, i) => (data.length - i - 1) * 100)
+        .attr('width', d => xScale(d.value))
+
+    chart.selectAll()
+      .data(data)
+      .enter()
+      .append('text')
+      .attr('data-index', (_, i) => i)
+      .attr('x', d => xScale(d.value))
+      .attr('y', d => yScale(d.name) + yScale.bandwidth() / 2)
+      .text(d => d.value)
+      .on('mouseover', handleMouseOver)
+      .on('mouseout', handleMouseOut);
+
+    chart.selectAll('.tick text')
+      .attr('data-index', (_, i) => i);
+  }, [rankings]);
+
+  return (
+    <div className="graph">
+      <h1>{rankings.name}</h1>
+      <svg width="1000" height="600" />
+      <p className="nulls"></p>
+    </div>
+  );
 }
 
 export default Graph;
