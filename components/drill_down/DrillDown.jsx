bc1ea738ff7e973cc31b6045f3f0b7be456a91ab
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-05 11:59:42 +0100

    Refactor DrillDown to use the provided data

--- components/drill_down/DrillDown.jsx
+++ components/drill_down/DrillDown.jsx
@@ -1,42 +1,32 @@
 import { useState } from "react";
-import axios from "axios";
 
-const DrillDown = () => {
-  const [links, setLinks] = useState([]);
+const DrillDown = ({ ancestry, setMember }) => {
+  const attributes = ancestry.attributes;
 
-  let sector = 'Mining';
-  let year = 2018;
-  let type = 'group';
-  let id = 3;
-
-  if (typeof window !== "undefined") {
-    const urlParams = new URLSearchParams(window.location.search);
-
-    sector = urlParams.get('sector');
-    year = urlParams.get('year');
-    type = urlParams.get('type');
-    id = urlParams.get('id');
+  const handleClick = (node) => {
+    return () => {
+      setMember({ type: node.type.toLowerCase(), id: node.id });
+    };
   }
 
-  axios.get(`http://localhost:3000/ancestry/${type}/${id}`)
-    .then(function (response) {
-      const d = response.data;
+  const children = ancestry.children.map(({ type, id }) => {
+    const attr = attributes[type][id];
+    const name = attr.name;
 
+    return { type, id, name };
+  });
 
-      const bar = d.children.map(({ type, id }) => {
-        const name = d.attributes[type][id].name;
-        const href = `?sector=${sector}&year=${year}&type=${type.toLowerCase()}&id=${id}`;
+  const link = (child, index) => (
+    <a key={index} className="link" onClick={handleClick(child)}>
+      {child.name}
+    </a>
+  );
 
-        return { name, href };
-      });
-
-
-      setLinks(bar);
-    })
-
-  return <nav>
-    {links.map((l, i) => <div key={i}><a href={l.href}>{l.name}</a></div>)}
-  </nav>;
+  return (
+    <div className="drill_down">
+      {children.map(link)}
+    </div>
+  );
 };
 
 export default DrillDown;
